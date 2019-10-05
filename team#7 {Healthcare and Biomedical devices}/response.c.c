a002f1bc88b56142973d935d6ce35c29c366b75d
rakshil <rakshilmodi14@gmail.com>
2019-10-05 22:42:35 +0530

    Update

---
+++ team#7 {Healthcare and Biomedical devices}/response.c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int respond;
+do
+{
+	
+	printf("Have you recieved the location of the accident \n");	
+	printf("Waiting For Respond from Hospital..Enter '1' for YES \n");
+	scanf("%d",&respond);
+
+}
+
+while(respond!=1);
+
+}
+
